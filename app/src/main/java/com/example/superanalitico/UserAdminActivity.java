80c2ce408aade6bd88334b929d6d28809421aa64
TomatoPana <mdlb.lobo@gmail.com>
2021-11-29 21:07:42 -0600

    Added new users activities

---
+++ app/src/main/java/com/example/superanalitico/UserAdminActivity.java
@@ -0,0 +1,14 @@
+package com.example.superanalitico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_admin);
+    }
+}
