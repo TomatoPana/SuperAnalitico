e361572d1c03f42c10868d0e6e2e746923ac9f21
TomatoPana <mdlb.lobo@gmail.com>
2021-12-03 14:16:37 -0600

    Añadido funcionalidad de login con firebase

--- app/src/main/java/com/example/superanalitico/MainActivity.java
+++ app/src/main/java/com/example/superanalitico/MainActivity.java
@@ -1,125 +1,136 @@
 package com.example.superanalitico;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.sql.Time;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences savedUserData;
-    boolean isLogged;
     EditText emailText;
     EditText passwordText;
     Button loginButton;
     FirebaseAuth mAuth;
+    boolean isLogged;
+    FirebaseFirestore mDatabase;
 
-    public void setLoginStatus(String email, String password) {
-        if(isLogged) {
-            emailText.setText(email);
-            emailText.setEnabled(false);
-            passwordText.setText(password);
-            passwordText.setEnabled(false);
-            loginButton.setText("Logout");
+    private void redirectLoggedUser(String Uid) {
+        final String TAG = "Firebase";
+        mDatabase = FirebaseFirestore.getInstance();
+        CollectionReference userCollection = mDatabase.collection("users");
+        DocumentReference userDocument = userCollection.document(Uid);
+        userDocument.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                assert document != null;
+                if (document.exists()) {
+                    Intent intent;
+                    switch (String.valueOf(Objects.requireNonNull(document.getData()).get("user_type"))) {
+                        case "user":
+                        default:
+                            intent = new Intent(MainActivity.this, UserActivity.class);
+                            break;
+                        case "analytic":
+                            intent = new Intent(MainActivity.this, UserAnalyticActivity.class);
+                            break;
+                        case "admin":
+                            intent = new Intent(MainActivity.this, UserAdminActivity.class);
+                            break;
+                    }
+                    startActivity(intent);
+                } else {
+                    Log.d(TAG, "No such document");
+                    // Create document with defaults values
+                }
+            } else {
+                Log.d(TAG, "get failed with ", task.getException());
+            }
+        });
+    }
+
+    public void firebaseLogin(String email, String password) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()){
+                        SharedPreferences.Editor writer = savedUserData.edit();
+                        writer.putString("email", email);
+                        writer.putString("password", password);
+                        writer.apply();
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        assert user != null;
+                        redirectLoggedUser(mAuth.getCurrentUser().getUid());
+                    }
+                });
+    }
+
+    private boolean verifyLoginStatus() {
+        mAuth = FirebaseAuth.getInstance();
+        savedUserData = getSharedPreferences("savedUserData", MODE_PRIVATE);
+        String savedEmail = savedUserData.getString("email", "");
+        String savedPassword = "";
+        if(savedEmail.length() == 0){
+            return false;
+        } else {
+            savedPassword = savedUserData.getString("password", "");
+            firebaseLogin(savedEmail, savedPassword);
         }
+        return (mAuth.getCurrentUser() != null);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         mAuth = FirebaseAuth.getInstance();
+        if(!verifyLoginStatus()){
+            setContentView(R.layout.activity_main);
+            loginButton = findViewById(R.id.button);
+            emailText = findViewById(R.id.editTextTextEmailAddress);
+            passwordText = findViewById(R.id.editTextTextPassword);
+            isLogged = false; // Only for pre initializing
 
-        loginButton = findViewById(R.id.button);
-        emailText = findViewById(R.id.editTextTextEmailAddress);
-        passwordText = findViewById(R.id.editTextTextPassword);
-        isLogged = false; // Only for pre initializing
-        savedUserData = getSharedPreferences("savedUserData", MODE_PRIVATE);
-        SharedPreferences.Editor dataWriter = savedUserData.edit();
-
-        String savedEmail = savedUserData.getString("email", "");
-        String savedPassword = "";
-        if(savedEmail.length() == 0){
-            isLogged= false;
-        } else {
-            savedPassword = savedUserData.getString("email", "");
-            isLogged = true;
-        }
-
-        setLoginStatus(savedEmail, savedPassword);
-
-        loginButton.setOnClickListener(view -> {
-            if(!isLogged) {
+            loginButton.setOnClickListener(view -> {
                 String email = emailText.getText().toString();
                 String password = passwordText.getText().toString();
-                if(email.length() == 0) {
+                if (email.length() == 0) {
                     emailText.setError("The Email is Required");
                     return;
                 }
-                if(password.length() == 0) {
+                if (password.length() == 0) {
                     passwordText.setError("The Password is Required");
                     return;
                 }
-
-                dataWriter.putString("email", email);
-                dataWriter.putString("password", password);
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("Login", "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    // Iniciar nueva actividad
-                                    Intent userActivity = new Intent(MainActivity.this, UserActivity.class);
-                                    startActivity(userActivity);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("Login", "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(MainActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    // mostrar error
-                                }
-                            }
-                        });
-
-            } else {
-                dataWriter.clear();
-            }
-            dataWriter.apply();
-
-
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null) {
-            // Obtener informacion del usuario e iniciar la actividad correspondiente
-            Intent userActivity = new Intent(MainActivity.this, UserActivity.class);
-            startActivity(userActivity);
+                firebaseLogin(email, password);
+                if(mAuth.getCurrentUser() != null) {
+                    Toast.makeText(this, "Incorrect User or Password", Toast.LENGTH_LONG).show();
+                }
+            });
         }
-
-        // Verificar SharedPreferences
-
     }
 }
